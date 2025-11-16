2c8af61d468723dfab85688945311e76e7a8b14c
Katrina Lu <katrinalu@Katrinas-MacBook-Pro-2024.local>
2025-11-16 14:36:58 -0800

    v1.3.7. Missing social history 2 images in previous push.

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ class ChineseLearningApp {
         this.calendar = null;
 
         // --- GLOBAL CONFIGURATION VARIABLES ---
-        this.APP_VERSION = '1.3.6';
+        this.APP_VERSION = '1.3.7';
         this.MAX_LEVEL = 22;
-        this.DEFAULT_WORDS_VERSION = '1.3.6';
-        this.LATEST_MINIGAME_VERSION = '1.3.6';
+        this.DEFAULT_WORDS_VERSION = '1.3.7';
+        this.LATEST_MINIGAME_VERSION = '1.3.7';
 
         this.REVIEW_WORDS_PER_SESSION = 20;
         this.REVIEW_CURRENT_LEVEL_COMPLETIONS = 1;
